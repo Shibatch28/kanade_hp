dee84114b06919a4b685c91c235fde3a7753c3e4
SIRIUS\shiba <mail@48inunokoya.net>
2025-10-18 01:25:48 +0900

    定数を管理するファイルを追加

---
+++ lib/models/consts.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:kanade_hp/models/social_link.dart';
+
+const SocialLink petitFourSite = SocialLink(
+  icon: Icons.music_note,
+  label: "Petit four 公式サイト",
+  url: "https://www.petitfour.online/",
+);
+const SocialLink petitFourX = SocialLink(
+  icon: Icons.music_note,
+  label: "Petit four 公式X",
+  url: "https://x.com/petitfour309",
+);
+const SocialLink kanadeX = SocialLink(
+  icon: Icons.people,
+  label: "近藤 奏 X個人アカウント",
+  url: "https://x.com/immortal_dirt5",
+);
+const SocialLink kanadeFacebook = SocialLink(
+  icon: Icons.facebook,
+  label: "近藤 奏 Facebook",
+  url: "https://www.facebook.com/profile.php?id=100032395710443",
+);
