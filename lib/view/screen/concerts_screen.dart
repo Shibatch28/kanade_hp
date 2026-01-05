bf325098955f8873941ded2c2b117a6e27822f67
Shibatch <lawnbone.tubist@gmail.com>
2026-01-05 21:45:43 +0900

    ✨ 2026年1月24日のコンサート情報を追加

--- lib/view/screen/concerts_screen.dart
+++ lib/view/screen/concerts_screen.dart
@@ -15,22 +15,30 @@ class ConcertsScreen extends StatelessWidget {
         children: [
           MainVisual(title: 'コンサート情報', imageDirectory: 'concerts', height: 200),
           ConcertInfo(
+            title: '近藤奏ピアノリサイタル D',
+            date: '2026年1月24日(土)',
+            time: '開場:13:20 開演:14:00',
+            venue: '江﨑ホール',
+            address: '静岡市葵区七間町8-20 毎日江﨑ビル9階',
+            flyerImagePaths: const ['assets/flyers/20260124a.png', 'assets/flyers/20260124b.png'],
+            programs: [
+              ProgramItem(composer: 'セルゲイ･プロコフィエフ', pieces: ['ピアノソナタ第6番 作品82', 'ピアノソナタ第8番 作品84']),
+            ],
+            ticketPrice: '全席 自由席 1,200円',
+            ticketInfo: 'チケットのお買い求めはチケットぴあをご利用ください。',
+            ticketOptions: ['チケットぴあ (P コード: 317675)'],
+          ),
+          ConcertInfo(
             title: '近藤奏 ピアノリサイタル',
             date: '延期',
             time: '開場:18:15 開演:19:00',
             venue: '下北沢アレイホール',
             address: '東京都世田谷区北沢2丁目24-8 下北沢アレイ 3F',
-            flyerImagePaths: const [
-              'assets/flyers/20251205a.jpeg',
-              'assets/flyers/20251205b.jpeg',
-            ],
+            flyerImagePaths: const ['assets/flyers/20251205a.jpeg', 'assets/flyers/20251205b.jpeg'],
             programs: [
               ProgramItem(
                 composer: 'ベートーヴェン',
-                pieces: [
-                  '交響曲第7番イ長調 作品92 第1, 2楽章 (リスト編曲)',
-                  'ピアノソナタ第 14番「月光」嬰ハ短調作品 27-2',
-                ],
+                pieces: ['交響曲第7番イ長調 作品92 第1, 2楽章 (リスト編曲)', 'ピアノソナタ第 14番「月光」嬰ハ短調作品 27-2'],
               ),
               ProgramItem(composer: 'ショパン', pieces: ['ワルツ・フォー・デビィ']),
               ProgramItem(composer: 'カプースチン', pieces: ['ブラジルの水彩画パラフレーズ']),
@@ -40,11 +48,7 @@ class ConcertsScreen extends StatelessWidget {
             ticketOptions: ['チケットぴあ（Pコード:311308）'],
           ),
           const SizedBox(height: 30),
-          const Text(
-            '過去のコンサート',
-            style: TextStyle(fontSize: 48),
-            textAlign: TextAlign.center,
-          ),
+          const Text('過去のコンサート', style: TextStyle(fontSize: 48), textAlign: TextAlign.center),
           ConcertInfo(
             title: '近藤奏 ピアノリサイタル「C」',
             date: '2025年10月4日(土)',
@@ -53,24 +57,14 @@ class ConcertsScreen extends StatelessWidget {
             address: '静岡市葵区七間町8-20 毎日江崎ビル9階',
             flyerImagePaths: const ['assets/flyers/flyer_C.png'],
             programs: [
-              ProgramItem(
-                composer: 'ショパン',
-                pieces: ['ピアノソナタ第3番 ロ短調 Op.58 第1楽章', 'ロンド 変ホ長調 Op.16'],
-              ),
+              ProgramItem(composer: 'ショパン', pieces: ['ピアノソナタ第3番 ロ短調 Op.58 第1楽章', 'ロンド 変ホ長調 Op.16']),
               ProgramItem(composer: 'ジョン・ケージ', pieces: ['4分33秒']),
               ProgramItem(composer: '千原英喜', pieces: ['雲丹長者の君の讃頌より']),
-              ProgramItem(
-                composer: '近藤奏',
-                pieces: ['Don\'t call (現代ピアノという6つの電子楽器を用いて)'],
-              ),
+              ProgramItem(composer: '近藤奏', pieces: ['Don\'t call (現代ピアノという6つの電子楽器を用いて)']),
             ],
             ticketPrice: '全席 自由席 1200円',
             ticketInfo: 'チケットのお買い求めはチケットぴあをそのほかプレイガイドをご利用ください。',
-            ticketOptions: [
-              'チケットぴあ（Pコード:307040）',
-              'すみやグッディ本店（葵区呉服町）',
-              'TEL:054-255-6722',
-            ],
+            ticketOptions: ['チケットぴあ（Pコード:307040）', 'すみやグッディ本店（葵区呉服町）', 'TEL:054-255-6722'],
           ),
           ConcertInfo(
             title: 'お寺今世音(こんさあと)',
@@ -78,17 +72,11 @@ class ConcertsScreen extends StatelessWidget {
             time: '開場:15:00 開演:16:00',
             venue: '南叟寺',
             address: '静岡県静岡市葵区足久保口組 1579',
-            flyerImagePaths: const [
-              'assets/flyers/20251115a.jpg',
-              'assets/flyers/20251115b.jpg',
-            ],
+            flyerImagePaths: const ['assets/flyers/20251115a.jpg', 'assets/flyers/20251115b.jpg'],
             programs: [
               ProgramItem(composer: 'ドビュッシー', pieces: ['映像第1集より「水の反映」']),
               ProgramItem(composer: 'サティ', pieces: ['ジムノペディ第1番']),
-              ProgramItem(
-                composer: '千原英喜',
-                pieces: ['星月夜34の音の画集より「夕暮れ時は逢魔が時」'],
-              ),
+              ProgramItem(composer: '千原英喜', pieces: ['星月夜34の音の画集より「夕暮れ時は逢魔が時」']),
               ProgramItem(composer: 'ラヴェル', pieces: ['亡き王女のためのパヴァーヌ']),
               ProgramItem(composer: '南こうせつ', pieces: ['まごころに生きる']),
               ProgramItem(composer: 'カプースチン', pieces: ['演奏会用エチュード第3番トッカティーナ']),
